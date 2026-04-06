4ea3402183cebbf9d5ed7255ddfe7d862214e539
Christina Wang <yimingwang1005@gmail.com>
2026-04-06 16:14:46 -0700

    added verification of generation order

--- optimal_generation.py
+++ optimal_generation.py
@@ -4,6 +4,7 @@ from transformers import AutoTokenizer, AutoModelForMaskedLM
 
 import networkx as nx
 import matplotlib.pyplot as plt
+import math
 
 # %%
 model_id = "answerdotai/ModernBERT-base"
@@ -45,7 +46,9 @@ print(blk_prob)
 G=nx.DiGraph()
 G.add_node(-1)
 for i,p in enumerate(blk_prob):
-  G.add_edge(-1,i,weight=float(f"{p.item():.4f}"))
+#   G.add_edge(-1,i,weight=float(f"{p.item():.4f}"))
+  G.add_edge(-1,i,weight=p.item())
+
 
 # %%
 unmasked_blk_prob=[]
@@ -79,7 +82,9 @@ print(unmasked_blk_prob)
 for i,blk in enumerate(unmasked_blk_prob):
   for j,p in enumerate(blk):
     if p!=None:
-      G.add_edge(i,j,weight=float(f"{p.item():.4f}"))
+    #   G.add_edge(i,j,weight=float(f"{p.item():.4f}"))
+      G.add_edge(i,j,weight=p.item())
+
 pos = nx.circular_layout(G)
 nx.draw(G,pos,with_labels=True,connectionstyle="arc3,rad=0.2")
 edge_labels = nx.get_edge_attributes(G, "weight")
@@ -91,4 +96,40 @@ nx.draw(mst,pos,with_labels=True)
 edge_labels=nx.get_edge_attributes(mst,"weight")
 nx.draw_networkx_edge_labels(mst,pos,edge_labels=edge_labels,label_pos=0.4)
 plt.show()
+# %%
+order = list(nx.dfs_preorder_nodes(mst, source=-1))
+print(order)
+print(edge_labels)
+sum_of_edge=sum(edge_labels.values())
+print(math.exp(sum_of_edge))
+# 7.038466780220017e-12
+
+# %%
+# verify probability of this generation order
+total_prob=1
+text="[MASK] [MASK] [MASK] [MASK] [MASK] [MASK]"
+inputs = tokenizer(text, return_tensors="pt")
+targets = tokenizer(expected_text, return_tensors="pt").input_ids
+outputs = model(**inputs, labels=targets)
+initial_ids=blocks[order[1]]
+for ids in initial_ids:
+    prob=torch.nn.functional.softmax(outputs.logits[0][ids],dim=0)
+    total_prob*=prob[targets[0][ids]]
+
+for i in range(1,len(order)-1):
+  ids=blocks[order[i]] # [3,4],[1,2]
+  masked_words=["[MASK]"]*4
+  for j in ids:
+    masked_words.insert(j-1,words[j-1])
+  text=" ".join(masked_words)
+  inputs=tokenizer(text,return_tensors="pt")
+  outputs=model(**inputs,labels=targets)
+  next_pair=blocks[order[i+1]] # [1,2] [5,6]
+  for j in next_pair:
+    prob=torch.nn.functional.softmax(outputs.logits[0][j],dim=0)
+    total_prob*=prob[targets[0][j]]
+# total_prob+=prob[]
+print(total_prob)
+# 7.0385e-12
+
 # %%
