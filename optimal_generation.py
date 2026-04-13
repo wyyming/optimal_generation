7179aa7e844b257e7010f33ce597e2a65cefe343
Christina Wang <yimingwang1005@gmail.com>
2026-04-13 13:45:51 -0700

    added mst of remembering previous depths

--- optimal_generation.py
+++ optimal_generation.py
@@ -6,18 +6,20 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import math
 
+from collections import defaultdict
+
 # %%
-model_id = "answerdotai/ModernBERT-base"
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForMaskedLM.from_pretrained(model_id)
+model_id="answerdotai/ModernBERT-base"
+tokenizer=AutoTokenizer.from_pretrained(model_id)
+model=AutoModelForMaskedLM.from_pretrained(model_id)
 
 # %%
 from datasets import load_dataset
-ds = load_dataset("roneneldan/TinyStories")
+ds=load_dataset("roneneldan/TinyStories")
 
 # %%
 def find_optimal_gen_order(expected_text,blk_sz=2):
-    targets = tokenizer(expected_text, return_tensors="pt").input_ids
+    targets=tokenizer(expected_text, return_tensors="pt").input_ids
     targets_main=targets[0][1:-1] # targets_main.shape --> [num of token]
     num_tokens=targets_main.shape[0]
 
@@ -25,21 +27,21 @@ def find_optimal_gen_order(expected_text,blk_sz=2):
     num_blocks=len(blocks)
 
     def compute_blk_logprob(input_ids):
-        outputs = model(input_ids=input_ids, labels=targets)
+        outputs=model(input_ids=input_ids, labels=targets)
         logits_main=outputs.logits[0][1:-1][:] # logits_main.shape --> [num of token, vocab size]
         # compute cross entropy for all tokens at once
         nll=torch.nn.functional.cross_entropy(logits_main, targets_main,reduction='none')
         # transform [num of tokens] to [num of blocks, block size] then sum across columns
-        remainder = num_tokens % blk_sz
-        if remainder != 0:
-            pad_size = blk_sz - remainder
-            nll = torch.cat([nll, torch.zeros(pad_size, device=nll.device)])
-        blk_logprob = -nll.view(num_blocks, blk_sz).sum(dim=1)
+        remainder=num_tokens % blk_sz
+        if remainder!=0:
+            pad_size=blk_sz-remainder
+            nll=torch.cat([nll, torch.zeros(pad_size, device=nll.device)])
+        blk_logprob=-nll.view(num_blocks, blk_sz).sum(dim=1)
         return blk_logprob
 
     # fully masked text
-    masked_input_ids = targets.clone()
-    masked_input_ids[0, 1:-1] = tokenizer.mask_token_id
+    masked_input_ids=targets.clone()
+    masked_input_ids[0, 1:-1]=tokenizer.mask_token_id
     blk_logprob=compute_blk_logprob(masked_input_ids)
     G=nx.DiGraph()
     G.add_node(-1)
@@ -48,38 +50,66 @@ def find_optimal_gen_order(expected_text,blk_sz=2):
 
     # reveal one block at a time
     for i in range(num_blocks):
-        revealed_input_ids = targets.clone()
-        revealed_input_ids[0, 1:-1] = tokenizer.mask_token_id
-        for pos in blocks[i]:
-            revealed_input_ids[0, pos + 1] = targets[0, pos + 1]
+        revealed_input_ids=masked_input_ids.clone()
+        for ind in blocks[i]:
+            revealed_input_ids[0][ind+1]=targets[0][ind+1]
         blk_logprob=compute_blk_logprob(revealed_input_ids)
         for j,lp in enumerate(blk_logprob):
             if j!=i:
                 G.add_edge(i,j,weight=lp.item())
 
     # find max spanning arborescence
-    pos = nx.circular_layout(G)
+    pos=nx.circular_layout(G)
     mst=nx.maximum_spanning_arborescence(G)
     # label node with words
     mst.nodes[-1]['label']="start"
     for i,blk in enumerate(blocks):
-        words=[tokenizer.decode(targets_main[pos]) for pos in blk]
+        words=[tokenizer.decode(targets_main[ind]) for ind in blk]
         mst.nodes[i]['label']=" ".join(words)
     node_labels=nx.get_node_attributes(mst,"label")
-    nx.draw(mst,pos,with_labels=True,labels=node_labels)
+    # nx.draw(mst,pos,with_labels=True,labels=node_labels)
+    nx.draw(mst,pos,with_labels=True)
     edge_labels=nx.get_edge_attributes(mst,"weight")
     nx.draw_networkx_edge_labels(mst,pos,edge_labels=edge_labels,label_pos=0.4)
     plt.show()
-    depths = nx.single_source_shortest_path_length(mst, source=-1)
-    max_depth = max(depths.values())
+    
+    # metrics
+    depths=nx.single_source_shortest_path_length(mst, source=-1)
+    max_depth=max(depths.values())
     print("depth of tree: ", max_depth)
+    levels=defaultdict(list)
+    for node, depth in depths.items():
+        levels[depth].append(node)
+    print("node by levels: ", dict(levels))
     order=list(nx.lexicographical_topological_sort(mst))
     print("generation order: ", order)
-# 0:The quick 1:brown fox 2:jumps over 3:the lazy 4:dog today
+    print("sum log likelihood: ", sum(edge_labels.values()))
+    
+    # find loglikelihood of remembering previous levels
+    rmb=nx.DiGraph()
+    rmb.add_node(-1)
+    level_input_ids=masked_input_ids.clone()
+    for nodes in levels.values():
+        for n in nodes:
+            if n==-1:
+                continue
+            for ind in blocks[n]:
+                level_input_ids[0][ind+1]=targets[0][ind+1]
+        blk_logprob=compute_blk_logprob(level_input_ids)
+        # keep original generation order
+        out_edges=mst.out_edges(nodes)
+        for u,v in out_edges:
+            rmb.add_edge(u,v,weight=blk_logprob[v].item())
+    nx.draw(rmb,pos,with_labels=True)
+    rmb_edge_labels=nx.get_edge_attributes(rmb,"weight")
+    nx.draw_networkx_edge_labels(rmb,pos,edge_labels=rmb_edge_labels)
+    plt.show()
+    print("sum log likelihood of remembering: ", sum(rmb_edge_labels.values()))
+
 
 # %%
-# text=ds["train"][0]["text"]
-text="The quick brown fox jumps over the lazy dog today"
+text=ds["train"][0]["text"]
+# text="The quick brown fox jumps over the lazy dog today"
 find_optimal_gen_order(text)
 
 # %%
@@ -93,9 +123,9 @@ for t in subset:
 # TODO: update order so that generation order is correct
 total_prob=1
 text="[MASK] [MASK] [MASK] [MASK] [MASK] [MASK] [MASK] [MASK] [MASK] [MASK]"
-inputs = tokenizer(text, return_tensors="pt")
-targets = tokenizer(expected_text, return_tensors="pt").input_ids
-outputs = model(**inputs, labels=targets)
+inputs=tokenizer(text, return_tensors="pt")
+targets=tokenizer(expected_text, return_tensors="pt").input_ids
+outputs=model(**inputs, labels=targets)
 initial_ids=blocks[order[1]]
 for ids in initial_ids:
     prob=torch.nn.functional.softmax(outputs.logits[0][ids],dim=0)
